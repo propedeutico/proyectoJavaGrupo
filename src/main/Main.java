aaa4043eb13f5716d4c33ba3bfdb42bb30a33937
EartFlath <profesortigerman@gmail.com>
2026-08-12 16:33:12 -0300

    Agrega ingreso del nombre del usuario

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,18 @@
 package main;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Proyecto Git Java");
-		System.out.println("Modificado desde Pop!_OS");
+		
+		 Scanner teclado = new Scanner(System.in);
+
+	        System.out.print("Ingrese su nombre: ");
+	        String nombre = teclado.nextLine();
+
+	        System.out.println("Hola " + nombre);
+
+	        teclado.close();
 	}
 
 }
